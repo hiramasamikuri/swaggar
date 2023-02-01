89fb8b9bbe236ed4c85e54f3258d4fc31d909361
atsu-h <hazemoto.atsuki.18@shizuoka.ac.jp>
2023-02-01 20:36:45 +0900

    ローカルでswaggarUIを動かせるように設定

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,9 +1,27 @@
 window.onload = function() {
+  // ここにコピペ
+  function get_url() {
+    // @see https://stackoverflow.com/questions/35914069/how-can-i-get-query-parameters-from-a-url-in-vue-js
+    let uri = window.location.href.split('?');
+    if (uri.length === 2) {
+      let vars = uri[1].split('&');
+      let getVars = {};
+      let tmp = '';
+      vars.forEach(function (v) {
+        tmp = v.split('=');
+        if (tmp.length === 2)
+          getVars[tmp[0]] = tmp[1];
+      });
+      return getVars.url;
+    }
+  }
+  // ここまでコピペ
   //<editor-fold desc="Changeable Configuration Block">
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    //url: "https://petstore.swagger.io/v2/swagger.json",
+    url: get_url(), // ←ここを書き換え
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
